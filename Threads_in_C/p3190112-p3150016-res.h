60e970555e19a822ee81028af099bd8e4f275f1e
Konstantinos Marko <kwstasmarko2001@gmail.com>
2022-11-03 15:08:40 +0200

    Add files via upload

---
+++ Threads_in_C/p3190112-p3150016-res.h
@@ -0,0 +1,31 @@
+//AUEB Operating Systems Project 2021-22
+
+//Konstantinos Marko 3190112
+//DImitrios Vovlas 3150016
+
+#define numTele 3
+#define numCash 2
+
+#define numSeats 10
+
+#define numZoneA 10
+#define numZoneB 20
+
+#define probZoneA 30
+
+#define costZoneA 30
+#define costZoneB 20
+
+#define numSeatLow 1
+#define numSeatHigh 5
+
+#define timeTeleLow 1
+#define timeTeleHigh 5
+
+#define timeSeatLow 5
+#define timeSeatHigh 13
+
+#define timeCashLow 4
+#define timeCashHigh 8
+
+#define probCardSuccess 90
